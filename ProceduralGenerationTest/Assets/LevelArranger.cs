da00540bfeb4c744e43d30543c52b6c94550b967
Void <hanimohammad00@gmail.com>
2023-02-28 21:02:52 +0100

    Text mesh pro + clean code + generate button

--- ProceduralGenerationTest/Assets/LevelArranger.cs
+++ ProceduralGenerationTest/Assets/LevelArranger.cs
@@ -12,41 +12,36 @@ public class LevelArranger : MonoBehaviour
     [SerializeField] 
     private Collider groundArea;
 
-    // [SerializeField] 
-    // private GameObject[] groundObjects;
-    // [SerializeField]
-    // private List<GameObject> wallsToSpawn;
-
     private void Start()
     {
         GenerateLevel();
     }
 
-    private void GenerateLevel() //find the bounds and spawn the walls accordingly (then also rotate)
+    
+    //need corner pieces
+    public void GenerateLevel() //find the bounds and spawn the walls accordingly (then also rotate)
     {
+        ResetWalls();
+        
         //top layer
-
-        Vector3 startPoint = groundArea.bounds.max;
-        startPoint.x = groundArea.bounds.min.x;
-        float distanceFromStart = 0;
-
-        List<GameObject> topWalls = GetCorrectWalls(groundArea.transform.localScale.x);
-
-        for (int i = 0; i < topWalls.Count; i++) //auto calculated the distance and distribute the walls
-        {
-            GameObject currentWall = topWalls[i];
-            Vector3 currentObjectScale = currentWall.transform.localScale;
-            Vector3 spawnPosition = startPoint + Vector3.right * (currentObjectScale.x/2f + distanceFromStart) + Vector3.up * currentObjectScale.y/2f;
-            Instantiate(currentWall, spawnPosition, Quaternion.identity, transform);
-            distanceFromStart += currentObjectScale.x;
-            //topWalls.RemoveAt(i);
-        }
+        Vector3 topStartPoint = groundArea.bounds.max;
+        topStartPoint.x = groundArea.bounds.min.x;
+        GenerateWall(topStartPoint, GetCorrectWalls(groundArea.transform.localScale.x), Vector3.right);
+        
+        //right layer
+        Vector3 rightStartPoint = groundArea.bounds.max;
+        rightStartPoint.z = groundArea.bounds.min.z;
+        GenerateWall(rightStartPoint, GetCorrectWalls(groundArea.transform.localScale.z), Vector3.forward);
 
         //left layer
-
-        //right layer
+        Vector3 leftStartPoint = groundArea.bounds.min;
+        leftStartPoint.y = groundArea.bounds.max.y;
+        GenerateWall(leftStartPoint, GetCorrectWalls(groundArea.transform.localScale.z), Vector3.forward);
 
         //bottom layer (can avoid)
+        Vector3 bottomStartPoint = groundArea.bounds.min;
+        bottomStartPoint.y = groundArea.bounds.max.y;
+        GenerateWall(bottomStartPoint, GetCorrectWalls(groundArea.transform.localScale.x), Vector3.right);
     }
 
     private List<GameObject> GetCorrectWalls(float distance)
@@ -60,16 +55,39 @@ public class LevelArranger : MonoBehaviour
             GameObject randomWall = wallObjects[Random.Range(0, wallObjects.Length)];
             currentWallDistance += randomWall.transform.localScale.x;
 
-            if (currentWallDistance > distance)
-            {
-                Vector3 tempScale = randomWall.transform.localScale;
-                tempScale.x = distance - (currentWallDistance - randomWall.transform.localScale.x);
-                randomWall.transform.localScale = tempScale;
-            }
+            // if (currentWallDistance > distance) //effects the prefab scale as well
+            // {
+            //     Vector3 tempScale = randomWall.transform.localScale;
+            //     tempScale.x = distance - (currentWallDistance - randomWall.transform.localScale.x);
+            //     randomWall.transform.localScale = tempScale;
+            // }
             listOfWalls.Add(randomWall);
             //Debug.Log("Current distance is: " + currentWallDistance);
         }
         
         return listOfWalls;
+    }
+
+    private void GenerateWall(Vector3 startPoint, List<GameObject> wallsToSpawn, Vector3 spawnDirection)
+    {
+        float distanceFromStart = 0;
+        Quaternion rotation = spawnDirection == Vector3.forward ?  Quaternion.Euler(0, 90, 0) : Quaternion.identity;
+        
+        for (int i = 0; i < wallsToSpawn.Count; i++) //auto calculated the distance and distribute the walls
+        {
+            GameObject currentWall = wallsToSpawn[i];
+            Vector3 currentObjectScale = currentWall.transform.localScale;
+            Vector3 spawnPosition = startPoint + spawnDirection * (currentObjectScale.x/2f + distanceFromStart) + Vector3.up * currentObjectScale.y/2f;
+            Instantiate(currentWall, spawnPosition, rotation, transform);
+            distanceFromStart += currentObjectScale.x;
+        }
+    }
+    
+    private void ResetWalls()
+    {
+        foreach (Transform obj in transform)
+        {
+            Destroy(obj.gameObject);
+        }
     }
 }
